b8c55b90fed3a53905b61001edc4c72eb078ff0f
Juan Ramon Castan <juanramoncastan@yahoo.es>
2025-11-24 13:36:20 +0100

    Cambio version 1.0 Variables en minuscula Creacion de un archivo temporal como origen

---
+++ bin/aptkey-gpg.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+# Version: 1.0
+
+############################################
+## Imported file must be a public key
+## 
+
+############################################
+
+KEY_DEPOT="/etc/apt/trusted.gpg.d"
+PRIVIL="sudo"
+GPG_TEST="gpg --dearmor --dry-run"
+GPG="gpg --dearmor -vo"
+
+function running()
+{
+    local temporal_key_file=""
+    local url_origen=${1} ## url_origen can be a "file://", "http://" or "ftp://"
+    local input_key_file=$(basename -- "${url_origen}")
+    local key_name="${input_key_file%.*}"
+    local temporal_key_file="/tmp/${input_key_file}"
+    ## Show info
+    echo -e "Getting $input_key_file from:"
+    echo -e "${url_origen}"
+    
+    if [[  ${url_origen} =~ ^http://.+ ]] || [[  ${url_origen} =~ ^ftp://.+ ]] ; then
+        ##  Download public key file
+        wget -qO "${temporal_key_file}" "${url_origen}"
+    elif [[ -e ${url_origen} ]]; then
+        cp ${url_origen} ${temporal_key_file}
+    fi
+    
+    if [[ -z $(cat "${temporal_key_file}" | ${GPG_TEST}  1> /dev/null) ]]; then
+        echo ""
+        (cat "${temporal_key_file}" \
+                            | ${PRIVIL} ${GPG} ${KEY_DEPOT}/${key_name}.gpg \
+                            && rm "${temporal_key_file}" \
+                            && echo -e "\n${key_name}.gpg into ${KEY_DEPOT}") \
+                            || echo -e "Error adquiring the key"
+    else
+        echo "Not valid url or file doesn't exists"
+    fi
+
+    exit 0
+}
+
+function help_message(){
+
+    cat <<EOF
+    
+####  $( basename ${0} ) ####
+Syntaxi: $( basename ${0} ) [options] URL
+  
+  URL   Can be http://, ftp:// or a path to a key file
+  
+  Options:
+    -D  Debug option. Normal user and output file in the working directory
+    -h  Show this message
+     
+EOF
+}
+
+
+OPTIONS=$(getopt  -o "hD" -n '$0' -- "$@")
+
+if [ $? != 0 ] ; then
+  exit 1 ;
+fi
+
+eval set -- "$OPTIONS"
+
+while true ; do 
+    case $1 in 
+            -h)
+                shift
+                help_message
+                exit 0
+                ;;
+            -D)
+                shift
+                PRIVIL=""
+                KEY_DEPOT="$(realpath .)"
+                ;;
+            --)
+                shift
+                running "${1}"
+                break
+                ;;
+            *)
+                shift
+                exit 1
+                ;;
+    esac
+done
+
+exit 0
+
+
+
+
+
